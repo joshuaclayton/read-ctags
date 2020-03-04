256f6a155d7e7ab12900bbaf2d3c510bbc1a40a1
Joshua Clayton <joshua.clayton@gmail.com>
2020-03-04 18:21:46 -0500

    Remove unused function
    What?
    =====
    
    This removes `uniqueList`, which is exported but never used.

--- src/System/Ctags/Internal.hs
+++ src/System/Ctags/Internal.hs
@@ -1,22 +1,17 @@
 module System.Ctags.Internal
-    ( uniqueList
-    , tagsContent
+    ( tagsContent
     , stdinContent
     ) where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import qualified Data.Bifunctor as BF
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.Set as Set
 import qualified Data.Text.Encoding.Error as T
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as T
 import System.Ctags.IO
 import System.Ctags.Types
 import qualified System.Directory as D
-
-uniqueList :: Ord a => [a] -> [a]
-uniqueList = Set.toList . Set.fromList
 
 tagsContent :: MonadIO m => m (Either TagSearchOutcome T.Text)
 tagsContent = readTagsFile =<< findTagsFile
